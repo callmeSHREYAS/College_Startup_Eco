0119d65404d703724275f6d047f29b82b70c25b6
callmeSHREYAS <shreyasvartak83@gmail.com>
2026-04-10 15:37:41 +0530

    Database conneted

---
+++ src/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+require("dotenv").config();
+
+const app = express();
+
+connectDB()
+
+// Middleware
+app.use(helmet());
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+
+// Health check
+app.get("/", (req, res) => {
+  res.json({ status: "ok", message: "College Startup Platform API is running" });
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
